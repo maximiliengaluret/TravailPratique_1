10d28fb73c5f32459f9ce96d7fa67d12e98a39ce
Maximilien Galuret <maximiliengaluret@MacBook-Pro-de-Maximilien.local>
2020-02-17 21:16:21 -0500

    Fin de la creation du slider et fin de l animation de l horloge debut de la gestion du responsive complet de la page

---
+++ mjs/animation_3.js
@@ -0,0 +1,43 @@
+var horloge = new Image();
+
+function init(){
+    horloge.src = './img/clock.png';
+  window.requestAnimationFrame(draw);
+}
+
+function draw() {
+
+  var ctx = document.getElementsByTagName('canvas')[1].getContext('2d');
+  ctx.globalCompositeOperation = 'destination-over';
+  ctx.clearRect(0,0,300,300); // effacer le canvas
+
+  ctx.fillStyle = 'rgba(0,0,0,0.2)';
+  ctx.strokeStyle = 'rgba(0,0,0,0.0)';
+  ctx.save();
+  ctx.translate(150,150);
+
+  //Secondes
+  var time = new Date();
+  ctx.rotate( ((2*Math.PI)/60)*time.getSeconds() + ((2*Math.PI)/60000)*time.getMilliseconds() );
+  ctx.translate(0,0);
+  ctx.fillRect(5,0,90,5); // Ombre
+
+
+
+var time = new Date();
+ctx.rotate( ((Math.PI)/20)*time.getSeconds() + ((2*Math.PI)/60000)*time.getMilliseconds() );
+ctx.translate(0,0);
+ctx.fillRect(5,0,90,5); // Ombre
+
+
+  
+
+  ctx.restore();
+
+ 
+  ctx.drawImage(horloge,0,0,300,300);
+
+  window.requestAnimationFrame(draw);
+}
+
+init();
